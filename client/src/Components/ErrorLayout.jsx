3f38ab896e3b14cbb0e53b65c2fe52dcc7d78fca
AbsorbingInfo <88964994+AbsorbingInfo@users.noreply.github.com>
2023-11-10 19:29:32 +0530

    Delete client directory

--- client/src/Components/ErrorLayout.jsx
+++
@@ -1,8 +0,0 @@
-
-const ErrorLayout = () => {
-  return (
-    <div className="text-xl font-bold flex justify-center my-20">Oops!! This is invalid URL</div>
-  )
-}
-
-export default ErrorLayout
--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import store from './store'
-import { Provider } from 'react-redux'
-import { router } from './App'
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-
-const root = ReactDOM.createRoot(document.getElementById("root"))
-
-root.render( <Provider store={store}><RouterProvider router={router} /></Provider>)
